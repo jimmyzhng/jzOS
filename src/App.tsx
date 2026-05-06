c2c7d46859aea1f81014f36fc255c38235d1feec
Jimmy <jimmyzhang1@hotmail.com>
2026-05-06 15:20:53 -0700

    Create taskbar structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import TaskBar from "./components/TaskBar/TaskBar"
 import Window from "./components/Window"
 
 
@@ -6,9 +7,10 @@ function App() {
   return (
     <div
       className="h-screen w-screen bg-cover bg-cover bg-center flex items-center justify-center overflow-hidden"
-      style={{ backgroundImage: "url('/xp4k.jpeg')" }}
+      style={{ backgroundImage: "url('/xp/wallpaper.jpeg')" }}
     >
       <Window />
+      <TaskBar />
     </div>
   )
 }
