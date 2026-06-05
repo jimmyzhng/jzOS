f97b6c79e43f23bdec8befa5424b3e0e7eae3790
Jimmy <jimmyzhang1@hotmail.com>
2026-06-04 20:29:19 -0700

    Add app store functions to work on

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -8,7 +8,13 @@ interface AppState {
     openApp: (newApp: any) => void;
     closeApp: (appId: string) => void;
     focusApp: (appId: string) => void;
+    minimizeApp: (appId: string) => void;
+    maximizeApp: (appId: string) => void;
+    restoreApp: (appId: string) => void;
+
 }
+
+const updateApp = (apps: any[], appId: string, changes: object) => apps.map(app => app.id === appId ? { ...app, ...changes } : app)
 
 export const useAppStore = create<AppState>((set) => ({
     apps: [],
@@ -16,7 +22,9 @@ export const useAppStore = create<AppState>((set) => ({
 
     openApp: (newApp) => set((state) => ({
         apps: [...state.apps, newApp],
-        focusedAppId: newApp.id
+        focusedAppId: newApp.id,
+        minimized: false,
+        maximized: false
     })),
 
     closeApp: (appId) => set((state) => {
@@ -33,5 +41,17 @@ export const useAppStore = create<AppState>((set) => ({
 
     focusApp: (appId) => set(() => ({
         focusedAppId: appId
-    }))
+    })),
+
+    minimizeApp: (appId) => set((state) => {
+
+
+    }),
+    maximizeApp: (appId) => set((state) => ({
+        apps: updateApp(state.apps, appId, { maximized: true })
+    })),
+    restoreApp: (appId) => set((state) => ({
+        apps: updateApp(state.apps, appId, { minimized: false, maximized: false })
+    })),
+
 }))
